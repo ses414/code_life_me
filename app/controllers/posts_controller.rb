fcfcda23db07aaa3e5cadc7d2bcdb67352e51cbe
ses414 <woodruffs1010@students.bowiestate.edu>
2017-11-09 11:46:25 -0500

    commit message

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -70,6 +70,7 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :author, :blog_entry)
-    end
-end
+      params.require(:post).permit(:title, :author, :blog_entry, :user_id)
+    end 
+  end  
+
